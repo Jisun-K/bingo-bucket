3736fd0f1b3037872d47b87067ba12394af51247
jisun <kjisun1221@gmail.com>
2025-11-05 21:25:44 +0900

    💄 기타 ui 수정

--- app/page.tsx
+++ app/page.tsx
@@ -18,8 +18,8 @@ export default function Home() {
         <div className="min-h-screen flex items-center justify-center p-4 mx-auto max-w-md">
             <div className="w-full">
                 <header className="text-center mb-8">
-                    <p className="mt-2 ">
-                        하나씩 이루며, 완성해가는 나의 빙고!
+                    <p className="mt-2 text-2xl">
+                        하나씩 채워가며, 빙고를 완성해보세요!
                     </p>
                 </header>
                 <BingoGrid boardId={boardId} />
